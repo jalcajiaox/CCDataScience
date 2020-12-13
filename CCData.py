969b2f102c3b0cd1084ed5e507d9ffe39f0652d6
unknown <mr.joraco@gmail.com>
2020-12-13 04:17:31 -0500

    Classes Buen Ejemplo para entenderlo

--- CCData.py
+++ CCData.py
@@ -1,32 +1,53 @@
-# create the initial variables below
-age=28
-sex=0
-bmi=26.2
-num_of_children=3
-smoker=0
+class Patient:
+  def __init__(self, name, age,sex,bmi,num_of_children,smoker):
+    self.name = name
+    self.age = age
+    self.sex= sex
+    self.bmi= bmi
+    self.num_of_children= num_of_children
+    self.smoker= smoker
+  def estimated_insurance_cost(self):
+    estimated_cost=250*self.age- 128*self.sex +370*self.bmi+425 * self.num_of_children+ 24000* self.smoker-12500
+    print("{}’s estimated insurance costs is {} dollars.".format(self.name,estimated_cost))
 
-# Add insurance estimate formula below
-insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
-print("This person's insurance cost is "+str(insurance_cost)+" dollars.")
+  def update_age(self,new_age):
+    self.estimated_insurance_cost()
+    return "{} is now {} years old.".format(self.name,new_age)
+  def update_num_children(self,new_num_children):
+    self.num_of_children= new_num_children
+    estimated_cost=250*self.age- 128*self.sex +370*self.bmi+425 * self.num_of_children+ 24000* self.smoker-12500
+    return "{} has {} children.".format(self.name,self.num_of_children)
+  def patient_profile(self):
+      patient_information = {}
+      patient_information["Name"] = self.name
+      patient_information["Age"] = self.age
+      patient_information["Sex"] = self.sex
+      patient_information["BMI"] = self.bmi
+      patient_information["Number of Children"] = self.num_of_children
+      patient_information["Smoker"] = self.smoker
+      return patient_information
 
-# Age Factor
-age+=4
-new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
-change_in_insurance_cost=new_insurance_cost-insurance_cost
-print("The change in cost of insurance after increasing the age by 4 years is "+ str(new_insurance_cost)+ " dollars.")
-# BMI Factor
-age=28
-bmi+=3.1
-new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
-change_in_insurance_cost=new_insurance_cost-insurance_cost
-print("The change in cost of insurance after increasing BMI by 3.1 is "+ str(new_insurance_cost)+ " dollars.")
-# Male vs. Female Factor
-bmi=26.2 
-sex=1
-new_insurance_cost=250*age-128*sex+370*bmi+425*num_of_children+24000*smoker-12500
-change_in_insurance_cost=new_insurance_cost-insurance_cost
-
-print("The change in estimated cost for being male instead of female is " +str(new_insurance_cost) +" dollars.")
+  
 
 
-# Extra Practice
+
+
+
+
+patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
+print(patient1.name)
+print(patient1.age)
+print(patient1.sex)
+print(patient1.bmi)
+print(patient1.num_of_children)
+print(patient1.smoker)
+
+print("\n")
+
+print(patient1.update_age(2))
+print("\n")
+print(patient1.update_num_children(2))
+print(patient1.patient_profile())
+
+
+    # add more parameters here
