dbd5b40f839d2c267b4fac9951f192a14ad16ca5
unknown <mr.joraco@gmail.com>
2020-12-16 00:23:25 -0500

    File Project Aprendiendo Files CSV, JSON..Read, Write

--- CCData.py
+++ CCData.py
@@ -1,53 +1,46 @@
-class Patient:
-  def __init__(self, name, age,sex,bmi,num_of_children,smoker):
-    self.name = name
-    self.age = age
-    self.sex= sex
-    self.bmi= bmi
-    self.num_of_children= num_of_children
-    self.smoker= smoker
-  def estimated_insurance_cost(self):
-    estimated_cost=250*self.age- 128*self.sex +370*self.bmi+425 * self.num_of_children+ 24000* self.smoker-12500
-    print("{}’s estimated insurance costs is {} dollars.".format(self.name,estimated_cost))
+import csv
 
-  def update_age(self,new_age):
-    self.estimated_insurance_cost()
-    return "{} is now {} years old.".format(self.name,new_age)
-  def update_num_children(self,new_num_children):
-    self.num_of_children= new_num_children
-    estimated_cost=250*self.age- 128*self.sex +370*self.bmi+425 * self.num_of_children+ 24000* self.smoker-12500
-    return "{} has {} children.".format(self.name,self.num_of_children)
-  def patient_profile(self):
-      patient_information = {}
-      patient_information["Name"] = self.name
-      patient_information["Age"] = self.age
-      patient_information["Sex"] = self.sex
-      patient_information["BMI"] = self.bmi
-      patient_information["Number of Children"] = self.num_of_children
-      patient_information["Smoker"] = self.smoker
-      return patient_information
+compromised_user=[]
 
-  
+with open ("passwords.csv",newline="") as password_file:
+  password_csv=csv.DictReader(password_file)
 
+  for row in password_csv:
+    compromised_user.append(row["Username"])
 
-
-
-
-
-patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
-print(patient1.name)
-print(patient1.age)
-print(patient1.sex)
-print(patient1.bmi)
-print(patient1.num_of_children)
-print(patient1.smoker)
+print(compromised_user)
 
 print("\n")
 
-print(patient1.update_age(2))
-print("\n")
-print(patient1.update_num_children(2))
-print(patient1.patient_profile())
+with open("compromised_users.txt","w") as compromised_user_file:
+  for x in compromised_user:
+   
+    compromised_user_file.write(x)
+
+import json
+
+with open("boss_message.json","w") as boss_message:
+  boss_message_dict={"recipient":"The Boss","message":"Mission Success"}
+  json.dump(boss_message_dict,boss_message)
+
+  with open("new_passwords.csv","w") as new_passwords_obj:
+    slash_null_sig=""" _  _     ___   __  ____             
+/ )( \   / __) /  \(_  _)            
+) \/ (  ( (_ \(  O ) )(              
+\____/   \___/ \__/ (__)             
+ _  _   __    ___  __ _  ____  ____  
+/ )( \ / _\  / __)(  / )(  __)(    \ 
+) __ (/    \( (__  )  (  ) _)  ) D ( 
+\_)(_/\_/\_/ \___)(__\_)(____)(____/ 
+        ____  __     __   ____  _  _ 
+ ___   / ___)(  )   / _\ / ___)/ )( \
+(___)  \___ \/ (_/\/    \\___ \) __ (
+       (____/\____/\_/\_/(____/\_)(_/
+ __ _  _  _  __    __                
+(  ( \/ )( \(  )  (  )               
+/    /) \/ (/ (_/\/ (_/\             
+\_)__)\____/\____/\____/"""
+    new_passwords_obj.write(slash_null_sig)  
 
 
-    # add more parameters here
+    
